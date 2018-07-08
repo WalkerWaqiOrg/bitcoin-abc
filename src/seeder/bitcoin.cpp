3c46c949ec459d01287a3c4c1bd505d51a0b6b13
WalkerWaqi <srrain@outlook.com>
2018-07-08 11:12:21 +0800

    Update netMagic for dns seeder

--- src/seeder/bitcoin.cpp
+++ src/seeder/bitcoin.cpp
@@ -13,7 +13,7 @@
 bool fTestNet;
 
 // The network magic to use.
-CMessageHeader::MessageMagic netMagic = {0xe3, 0xe1, 0xf3, 0xe8};
+CMessageHeader::MessageMagic netMagic = {0x72, 0x61, 0x69, 0x6e};
 
 #define BITCOIN_SEED_NONCE 0x0539a019ca550825ULL
 
